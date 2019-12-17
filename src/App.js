02804a8c5511a0f1ab73a15a7b3f582f8d54d501
Vincent Sanders <vincesanders@hotmail.com>
2019-12-16 18:12:49 -0600

    Added Components to app.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    <Route exact path='/'></Route>
-    <Route path='/about'></Route>
-    <Route path='/contact'></Route>
+    <Route exact path='/'>
+      <Home />
+    </Route>
+    <Route path='/about'>
+      <About />
+    </Route>
+    <Route path='/contact'>
+      <Contact />
+    </Route>
   </div>
 );
 
